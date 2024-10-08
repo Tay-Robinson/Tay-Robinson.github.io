26d048300a6e4db7a516214f60d19e8d17210c04
Tay-Robinson <tayshaun.robinson@ndus.edu>
2024-10-08 13:38:04 -0500

    Add files via upload

---
+++ hello/code.js
@@ -0,0 +1,7 @@
+
+document.getElementById("b1").addEventListener("click", onclick)
+
+function onclick () { 
+    document.getElementById("Heading").innerHTML = document.getElementById("data").value 
+}
+
